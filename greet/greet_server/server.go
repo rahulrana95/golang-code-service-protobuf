1b297216ab3b685f6784af1c987cca030b92c97b
Rahul Rana <rahulrana@uber.com>
2021-12-28 13:44:53 +0530

    Listening server for service

--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/rahulrana95/grpc-go-course/greet/greetpb"
+	"google.golang.org/grpc"
+)
+
+type server struct {
+}
 
 func main() {
-	fmt.Println("Hello")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:5000")
+	log.Println("Listening server at", 5000)
+	if err != nil {
+		fmt.Println("Not able to start server")
+		log.Fatalf("FAiled to listen", err)
+	}
+
+	s := grpc.NewServer()
+
+	greetpb.RegisterGreetServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve", err)
+	}
+
 }
